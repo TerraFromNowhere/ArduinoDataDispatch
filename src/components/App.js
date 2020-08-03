01bbeb84e669053471b5c306368ad79a94990996
unknown <raspred@KONSTRUCTOR10.WGC.local>
2020-08-03 16:51:11 +0400

    Couple changes in package.json,App.js work properly now

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React , {Component} from "react";
 
 const App = () =>{
-    return (<div>Hello</div>);
+
+    return (
+        <div style={{display:"flex",backgroundColor:"red",width:"100px",height:"100px",justifyContent:"center",alignItems:"center"}}>
+             <div style={{fontWeight:"bold"}}>Hello</div>
+        </div>
+       
+    );
+
 }
 
 export default App;
