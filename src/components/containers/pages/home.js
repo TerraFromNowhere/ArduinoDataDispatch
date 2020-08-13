929f7e5858cab963a3876d86763533cbfc2a5037
TerraFromNowhere <frigadb789@gmail.com>
2020-08-13 16:43:17 +0400

    Hook states from component(it works xd)

--- src/components/containers/pages/home.js
+++ src/components/containers/pages/home.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React , {useState} from 'react';
+import {getData} from '../../dataHandlers/firebase';
+import {getDataPerUnitOfTime} from '../../dataHandlers/query';
 
    
-export const Home = (stateData,keys,getDataPerUnitOfTime) => {
+export const Home = () => {
 
+    let [data,stateData] = useState([]);
+    let [dataKeys,keys] = useState([]);
 
     return (
 
         <div>
 
-        <div style={{display:"flex",justifyContent:"center", width:""}}> <button style ={{width:"190px",height:"130px",color:"orange",fontSize:"30px"}} onClick = {()=>{getData().ref("DATA/2020/Month_8/Hour_10/SENSOR_1").once("value").then(items=>{
-            stateData(items.val());
-            keys(Object.keys(items.val()));
-        })}}>GET DATA</button>
+        <div style={{display:"flex",justifyContent:"center", width:""}}> <button style ={{width:"",height:"",color:"orange",fontSize:"20px"}} onClick = {()=>{getData().ref("DATA/2020/Month_8/Hour_10/SENSOR_1").once("value").then(items=>{
+            getDataPerUnitOfTime(stateData,keys,`DATA/2020/Month_8/Hour_10/SENSOR_1`) 
+                    })}}>GET DATA</button>
         <button>GET REAL TIME DATA  (refresh every n second)</button>
         <button onClick={()=>{getDataPerUnitOfTime(stateData,keys,`DATA/2020/Month_8/Hour_10/SENSOR_1`)}}>GET DATA PER HOUR</button>
         <button>GET DATA PER DAY</button>
@@ -19,7 +22,7 @@ export const Home = (stateData,keys,getDataPerUnitOfTime) => {
         <button onClick={()=>{console.log(dataKeys)}}>Log my state</button> </div>
         
         {
-
+ 
             dataKeys.map((item,i)=>{
 
                 return <div style={{border:" 2px solid red"}} key = {i}>
@@ -29,7 +32,7 @@ export const Home = (stateData,keys,getDataPerUnitOfTime) => {
                 <div> Humidity : {data[item].Humidity}%      </div>
                 <div> Voltage : {data[item].Voltage}V        </div>
         
-               </div>
+               </div> 
         })
         }
 
