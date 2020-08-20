c3062ddbb55c2b92a30d944c846575de0624d9b3
TerraFromNowhere <frigadb789@gmail.com>
2020-08-20 16:46:18 +0400

    Real time mode imitation via setInterval

--- src/components/dataHandlers/query.js
+++ src/components/dataHandlers/query.js
@@ -7,8 +7,28 @@ export const getDataPerUnitOfTime = (stateData,keys,queryString = String) =>{
         stateData(items.val());
         keys(Object.keys(items.val()));
    }).catch(e =>{
-       throw new Error(`Error ${e}`)
+       throw new Error(`Error ${e}`);
    });
 
+}
+
+export const realTimeQueryImitation = (stateData,keys,queryString = String) => {
+    console.log("Start to fetching data..."); 
+   return  setInterval(()=>{
+
+        console.log("Start to fetching data..."); 
+
+        return getData().ref(queryString).limitToLast(1).once('value').then(items =>{
+            stateData(items.val());
+            keys(Object.keys(items.val()));
+       }).catch(e =>{
+           throw new Error(`Error ${e}`);
+       });
+
+       console.log("Data fetched from sensor_1");
+
+    },60000);
+
+    let stop = clearInterval(timer);
 }
 
