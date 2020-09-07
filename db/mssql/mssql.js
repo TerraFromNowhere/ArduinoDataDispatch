3ec8002aaf58dc0457e67f1870ebc551f16aa6c0
TerraFromNowhere <frigadb789@gmail.com>
2020-09-07 16:29:01 +0400

    Connection methods to interact with mssql db,also  data handling modified in server file

--- db/mssql/mssql.js
+++ db/mssql/mssql.js
@@ -1,18 +1,28 @@
 const sql = require('mssql');
 
-const  sqlConnect = () => {
+const mssqlQueryString  = (id,belonging_to,temperature,humidity,voltage,timestamp,year,month,week,day,hour) => {
+    return `INSERT INTO dbo.Sensor_${id} (sensor_id,belonging_to,temperature,humidity,voltage,timestamp,year,month,week,day,hour)
+    values('${id}','${belonging_to}','${temperature}','${humidity}','${voltage}','${timestamp}','${year}','${month}','${week}','${day}','${hour}')`
+}
+
+const  mssqlConnectDataPusher = (queryString) => {
 
     const cred = require('../data/sqlCredentials.js');
 
-    return sql.connect(cred).then(pool => {
-        console.log(pool);
+    return sql.connect(cred).then(async pool => {
+        console.log("Data sended to mssql");
+        await pool.request().query(queryString);
+        
     }).catch(error => {
+        console.log("Unable to push data in db");
         console.log(error);
     });
 
 }
 
-module.exports = sqlConnect;
+
+module.exports = {mssqlQueryString,mssqlConnectDataPusher};
+
 
 
 
