27e27a33512aec2e6ab1577a35c52c8210105b96
TerraFromNowhere <frigadb789@gmail.com>
2020-09-11 14:31:08 +0400

    Domain adress cutted to separated config. file, new client fetcher

---
+++ configuration.js
@@ -0,0 +1,4 @@
+ const __SERVER_IP = '192.168.0.95';
+ const __PORT = 80;
+
+ module.exports = {__SERVER_IP,__PORT};
---
+++ src/components/dataHandlers/fetchData.js
@@ -0,0 +1,12 @@
+
+const __SERVER_IP = require('../../../configuration.js').__SERVER_IP;
+
+export const fetchData = (unitOfTime,setSpecialData,id) => {
+    
+    fetch(`http://${__SERVER_IP}/${unitOfTime}/${id}`)
+    .then(result => {
+        result.json().then(res => {setSpecialData(res)})
+    })
+    .catch(e => {console.log(e)});
+    
+}
