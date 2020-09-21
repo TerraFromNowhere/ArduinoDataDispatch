1b8ba41fdad01f4c942a4ee24628ea209ca5a208
unknown <@WGC.LOCAL>
2020-09-21 14:12:18 +0400

    Few changes for production running

--- configuration.js
+++ configuration.js
@@ -1,4 +1,4 @@
- const __SERVER_IP = '192.168.0.95';
- const __PORT = 80;
+ const __SERVER_IP = '192.168.0.2';
+ const __PORT = 9000;
 
  module.exports = {__SERVER_IP,__PORT};
--- src/components/dataHandlers/fetchData.js
+++ src/components/dataHandlers/fetchData.js
@@ -1,9 +1,14 @@
 
 const __SERVER_IP = require('../../../configuration.js').__SERVER_IP;
+const __PORT = require('../../../configuration.js').__PORT;
 
 export const fetchData = (unitOfTime,setSpecialData,id) => {
+
+  
     
-    fetch(`http://${__SERVER_IP}/${unitOfTime}/${id}`)
+    fetch(`http://${__SERVER_IP}:${__PORT}/${unitOfTime}/${id}`,{headers:{
+        'Access-Control-Allow-Origin': '*'
+    }})
     .then(result => {
         result.json().then(res => {setSpecialData(res)})
     })
