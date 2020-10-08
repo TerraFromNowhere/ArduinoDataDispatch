7494b05deb332daae5ac77946e248fe4f9334787
TerraFromNowhere <frigadb789@gmail.com>
2020-10-08 14:19:21 +0400

    Main reducer, store config.

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import {createStore,combineReducers,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import {mainReducer} from '../reducers/reducers';
 
 const middlewares = [thunk];
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({mainReducer});
 export const store = createStore(rootReducer,applyMiddleware(...middlewares));
