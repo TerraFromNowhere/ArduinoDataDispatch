d6d285dc4f853966702366e3fc8003788920103e
TerraFromNowhere <frigadb789@gmail.com>
2020-10-08 14:21:20 +0400

    Reducer pre formed

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,5 +1,17 @@
-const fetcher = (state = initialState,action) => {
+
+import * as action from '../actions/actions';
+
+const initialState = {
+    data : [],
+    isFetching:false,
+    id : null
+};
+
+export const mainReducer = (state = initialState,action) => {
     switch(action.type){
-        
+        case "FETCH_DATA" :
+            return dispatch(action.payload())
+        default :
+            return {...state}           
     }
 }
