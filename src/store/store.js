bf4db4268ce8a2c023b0bdf26274cea8f196c628
TerraFromNowhere <frigadb789@gmail.com>
2020-10-09 16:20:40 +0400

    Dev. tools composer

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import {createStore,combineReducers,applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {mainReducer} from '../reducers/reducers';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 const middlewares = [thunk];
 const rootReducer = combineReducers({mainReducer});
-export const store = createStore(rootReducer,applyMiddleware(...middlewares));
+export const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(...middlewares)));
