2a47380bffe4dc501d20e4368988c98089e02058
TerraFromNowhere <frigadb789@gmail.com>
2020-10-12 08:37:03 +0400

    Internal server fetching

--- src/components/dataHandlers/fetchData.js
+++ src/components/dataHandlers/fetchData.js
@@ -1,11 +1,16 @@
 
 const __SERVER_IP = require('../../../configuration.js').__SERVER_IP;
+const __PORT = require('../../../configuration.js').__PORT;
 
 export const fetchData = (unitOfTime,setSpecialData,id) => {
-    
-    fetch(`http://${__SERVER_IP}/${unitOfTime}/${id}`)
+
+      
+    fetch(`http://192.168.0.2:9000/${unitOfTime}/${id}`,{headers:{
+        'Access-Control-Allow-Origin': '*'
+    }})
     .then(result => {
         result.json().then(res => {setSpecialData(res)})
     })
-    .catch(e => {console.log(e)});    
+    .catch(e => {console.log(e)});
+    
 }
